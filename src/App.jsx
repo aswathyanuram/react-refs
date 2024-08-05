e1ef7b78e11b1f22b58f4eb329352fd64cd3224d
Jeeva Kalaiselvam <jeevak001@outlook.com>
2024-08-05 18:24:29 +0530

    Dashboard Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,67 @@
+import { useState } from "react";
 import "./App.css";
+import CounterPractise from "./projects/1/practise/CounterPractise";
+import CounterReference from "./projects/1/reference/CounterReference";
 
 function App() {
+  const projects = [
+    {
+      id: 1,
+      title: "Counter",
+      reference: <CounterReference />,
+      practise: <CounterPractise />,
+    },
+  ];
+
+  const TAB_PRACTISE = "TAB_PRACTISE";
+  const TAB_REFERENCE = "TAB_REFERENCE";
+
+  const [selectedProject, setSelectedProject] = useState(projects?.[0]);
+  const [selectedContentTab, setSelectedContentTab] = useState(TAB_REFERENCE);
+
   return (
     <div className="container">
-      <div className="leftsidebar">LEFT</div>
-      <div className="content">MAIN</div>
+      <div className="leftsidebar">
+        {projects.map((project) => {
+          return (
+            <div className="tab" key={project.id}>
+              <div
+                onClick={() => {
+                  setSelectedProject(project);
+                }}
+              >
+                {project.title}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="content">
+        <div className="content-buttons">
+          <div
+            className="content-button"
+            onClick={() => {
+              setSelectedContentTab(TAB_REFERENCE);
+            }}
+          >
+            Reference
+          </div>
+          <div
+            className="content-button"
+            onClick={() => {
+              setSelectedContentTab(TAB_PRACTISE);
+            }}
+          >
+            Practise
+          </div>
+        </div>
+        {selectedContentTab == TAB_REFERENCE && (
+          <div className="content">{selectedProject?.reference}</div>
+        )}
+        {selectedContentTab == TAB_PRACTISE && (
+          <div className="content">{selectedProject?.practise}</div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/projects/1/practise/CounterPractise.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CounterPractise() {
+  return <div>CounterPractise</div>;
+}
