27875579a28b05a6d72cb464bcd1c6ff4c7b270d
Aswathy <aswathyanuramar@gmail.com>
2024-08-08 18:23:24 +0530

    Notes Added

--- src/projects/6/practise/NotesPractise.jsx
+++ src/projects/6/practise/NotesPractise.jsx
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { ColorPicker } from "antd";
 
 export default function NotesPractise() {
-  return <div>NotesPractise</div>;
+  const [value, setValue] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [color, setColor] = useState("rgb(255,0,0)");
+  return (
+    <div>
+      <div>
+        <input
+          value={value}
+          placeholder="Add your notes"
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <ColorPicker
+          defaultValue="#1677ff"
+          value={color}
+          onChange={(e) => {
+            setColor(`rgb(${e.metaColor.r},${e.metaColor.g},${e.metaColor.b})`);
+          }}
+        />
+        <button
+          onClick={() => {
+            setNotes((old) => {
+              return [...old, { text: value, color: color }];
+            });
+          }}
+        >
+          Save
+        </button>
+      </div>
+      <div>
+        {notes.map((note) => {
+          return (
+            <div style={{ backgroundColor: note.color }}>{note.text} </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- src/projects/6/reference/NotesReference.jsx
+++ src/projects/6/reference/NotesReference.jsx
@@ -1,5 +1,61 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import styles from "./NotesReference.module.css";
+import { ColorPicker } from "antd";
 
 export default function NotesReference() {
-  return <div>Notes</div>;
+  const [value, setValue] = useState("");
+  const [notes, setNotes] = useState([]);
+  const ref = useRef(null);
+
+  const COLORS = ["red", "green", "blue"];
+
+  const [color, setColor] = useState(`rgb(255,255,255)`);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.data}>
+        <input
+          ref={ref}
+          className={styles.input}
+          type="text"
+          placeholder="Add your notes"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <ColorPicker
+          defaultValue="#1677ff"
+          value={color}
+          onChange={(e) => {
+            let inner = e.metaColor;
+            setColor(`rgb(${inner.r},${inner.g},${inner.b})`);
+          }}
+        />
+        <button
+          className={styles.button}
+          onClick={() => {
+            setNotes((old) => {
+              return [...old, { note: value, color: color }];
+            });
+            setValue("");
+            if (ref?.current) {
+              ref?.current?.focus();
+            }
+          }}
+        >
+          Save
+        </button>
+      </div>
+      <div className={styles.notes}>
+        {notes.map((note) => {
+          return (
+            <div className={styles.card} style={{ background: note.color }}>
+              {note.note}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
